817177c9a64f48acfdce4e4f5c89710cc8af9ce8
Parth Suthar <parth1926@gmail.com>
2025-03-27 10:10:12 -0400

    Update reefscape_config.js to use humber

--- 2025/reefscape_config.js
+++ 2025/reefscape_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2025onsca",
+      "defaultValue": "2025ontor",
       "disabled": "true",
       "required": "true"
     },
