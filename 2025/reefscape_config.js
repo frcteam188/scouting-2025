28f26d6b1a377d0f7918587ed9433d41a53bd0b7
Parth Suthar <parth1926@gmail.com>
2025-04-03 00:37:48 -0400

    Update reefscape_config.js

--- 2025/reefscape_config.js
+++ 2025/reefscape_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2025onham",
+      "defaultValue": "2025oncmp2",
       "disabled": "true",
       "required": "true"
     },
