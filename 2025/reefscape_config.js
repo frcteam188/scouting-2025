f7392d0f03b4b87699845df9305f2e5a80607bed
Parth Suthar <parth.suthar@taplytics.com>
2025-04-17 09:47:30 -0400

    Update reefscape_config.js

--- 2025/reefscape_config.js
+++ 2025/reefscape_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2025hop",
+      "defaultValue": "2025new",
       "disabled": "true",
       "required": "true"
     },
